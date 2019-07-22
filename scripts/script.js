84d62cea80648c17d95f2d8f94dd5095b7b00b5f
Lauren Mooney <49599773+laurennmooney@users.noreply.github.com>
2019-07-22 16:50:29 -0400

    first commit for first Javascript lab

---
+++ scripts/script.js
@@ -0,0 +1,38 @@
+let name = "Lauren Mooney";
+let age = 26;
+let birthday = "February 2";
+let detroitGC = true;
+let lifeEvents = ["I grew up in Lincoln Park, MI.", "I graduated from UDM with a BS in biology.","I attended Chris Cornell's last show in Detroit.","My PR on the leg press is 320 lbs." ];
+
+
+// if/else
+if (detroitGC) {
+    console.log(`My name is ${name} and I am a student at Grand Circus in Detroit, Michigan. I am currently ${age} years old and my birthday is on ${birthday}.`);
+} else {
+    console.log(`My name is ${name} and I am a student at Grand Circus in Grand Rapids, Michigan. I am currently ${age} years old and my birthday is on ${birthday}.`);
+}
+
+// for loop
+for (let i = 0; i < lifeEvents.length; i++) {
+    console.log(lifeEvents[i]);
+}
+
+// random number
+let randomNumber = Math.floor(Math.random() * 10);
+
+//counter 
+let counter = 0;
+
+// while loop
+while (true) {
+    if (randomNumber !== 5) {
+        console.log(`${randomNumber} !== 5`);
+        randomNumber = Math.floor(Math.random() * 11);
+        counter++;
+    } else {
+        counter++;
+        console.log(`5 === 5. It took ${counter} iterations to randomly generate the number 5.`);
+        break;
+    }
+}
+
